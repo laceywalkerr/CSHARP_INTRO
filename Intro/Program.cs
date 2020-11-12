ac188083b562a251f5179f06494371583d8a948e
Lacey Walker <bluelarksong@gmail.com>
2020-11-12 13:52:38 -0600

    added some comments defining the code

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿//in javascript, this would be the import
+using System;
 
 namespace Intro
 {
     public class Program
-    {
+    { //below is a method, the method name is Main
+        // "(string[] args)" are the parameters
         public static void Main(string[] args)
-        {
+        { //Console.Write(); is the new Console.Log();
             Console.Write("What is your name? ");
             string name = Console.ReadLine();
             Console.WriteLine($"Hello, {name}! I'm glad to meet you.");
